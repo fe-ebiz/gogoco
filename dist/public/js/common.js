cb7f8bb779503a5fc4ed04ee3f67a5e4421e39fb
vxggangxv <vxggangxv@naver.com>
2019-03-26 16:38:21 +0900

    Initial commit

---
+++ dist/public/js/common.js
@@ -0,0 +1,56 @@
+$(function() {
+
+	popClsFn();
+	datePick();
+	ascending();
+	tabFn();
+
+});
+
+// 팝업 기능 등록
+var popFn = {
+	show: function(obj_name) {
+		$(obj_name).show();
+	}
+}
+
+// 팝업 닫기 기능
+function popClsFn() {
+	var containerName = ".pop-container",
+		popContainer = $(containerName),
+		btn_cls = popContainer.find(".fn-cls");
+
+	btn_cls.on("click", function() {
+		$(this).closest(containerName).hide();
+	});
+	popContainer.find(".bg-back").on("click", function() {
+		$(this).closest(containerName).hide();
+	});
+}
+
+// 달력 ui
+function datePick() {
+	$( "input.date-ui" ).datepicker({
+		dateFormat: 'yy-mm-dd'
+	});
+}
+
+// 결과 오름차순 토글
+function ascending() {
+	$(".order-ui").on("click", function() {
+		$(this).toggleClass("ascending");
+	});
+}
+
+// 탭 기능
+function tabFn() {
+	var box = "";
+	$(".tab-list > .tab").on("click", function() {
+		var idx = $(this).index();
+		box = $(this).closest(".fn-tabShow");
+
+		$(this).addClass("on").siblings().removeClass("on");
+		box.find(".item-list > div").eq(idx).addClass("on").siblings().removeClass("on");
+		box.find(".item-list > div").eq(idx).show().siblings().hide();
+	});
+}
