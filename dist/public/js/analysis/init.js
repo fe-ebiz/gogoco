71a3b473215320470a9e4a655d40273c6415a355
s-rainkin <45417223+s-rainkin@users.noreply.github.com>
2019-03-29 16:13:16 +0900

    작년도 매출 비교 업데이트

--- dist/public/js/analysis/init.js
+++ dist/public/js/analysis/init.js
@@ -1,29 +1,48 @@
 $(function() {
+    /* 함수실행 */
+    yearCal();
+    overLastYearChart();
+    
+});
+
+/* 달력 - 년도별 */
+function yearCal(){
+    var yearTxt = new Array();
+        
+    $('input.type-year').yearpicker();
+}
+
+/* 테이블 & 차트 */
+function overLastYearChart() {
     var year = ['2018', '2019'],
         label = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
         dataPv = [71571690, 54442025, 49900652, 82223360, 82223360, 82223360, 82223360, 82223360, 82223360, 82223360, 82223360, 82223360, 82223360, 143815040],
         dataNt = [141061577, 133769949, 121899293, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     
     var chart = $('#chart'),
-        chartTit = $('.grid-chart-container .tit-chart'),
-        tblHd = $('.grid-tbl-container .grid-tbl .theadgroup'),
-        tblCon = $('.grid-tbl-container .grid-tbl .tbodygroup');
-
-    //chart 년도 추가 
-    chartTit.append('('+year[0]+'-'+year[1]+')');
-    //table
-    //년도 추가
-    for( var y = 0 ; y < year.length ; y++ ){
-        tblHd.find('th:eq('+(y+1)+') .cell').text(year[y]);
+        tblHd = $('.grid-tbl-container .grid-head .grid-tbl .rowgroup'),
+        tblConArea = $('.grid-tbl-container .grid-contents'),
+        tblCon = tblConArea.find('.grid-tbl .rowgroup');
+    
+    // thead
+    var hdHtml = "";
+    hdHtml += "<tr>";
+    hdHtml += '<th><div class="cell">월</div></th>';
+    for ( var y=0 ; y < year.length ; y++ ) {
+        hdHtml += '<th><div class="cell">' + year[y] + '</div></th>';
     }
-    //table contents 태그생성
-    var td = '<td><div class="cell"></div></td>';
+    hdHtml += "/<tr>";
+    tblHd.append(hdHtml);
+    
+    // tbody
     for( var i = 0 ; i < label.length ; i++ ){
-        tblCon.append('<tr>'+ td + td + td +'</tr>');
+        tblCon.append('<tr><td><div class="cell">' + label[i] + '</div></td><td><div class="cell">' + Number(dataPv[i]).toLocaleString() + '</div></td><td><div class="cell">' + Number(dataNt[i]).toLocaleString() + '</div></td></tr>');
     }
-    //금액 추가
-    for( var j = 0 ; j < label.length ; j++ ){
-        tblCon.find('tr:eq('+j+')').find('td:eq(0) .cell').text(label[j]).end().find('td:eq(1) .cell').text(dataPv[j].toLocaleString()).end().find('td:eq(2) .cell').text(dataNt[j].toLocaleString());
+
+    //table scoll y
+    if( tblConArea.height() < tblCon.height() ) {
+        $('.grid-tbl-container .grid-area').css({'border-right-width' : 1+'px'});
+        tblHd.parents('.grid-scrollable').css({'padding-right' : 17+'px'});
     }
     
     //chart data
@@ -41,8 +60,6 @@ $(function() {
     };
     
     window.onload = function() {
-        
-        
         //chart
         var ctx = chart.get(0).getContext('2d'); 
         
@@ -53,7 +70,11 @@ $(function() {
                 maintainAspectRatio: false,
                 scales: {
                     xAxes: [{ 
-                        gridLines: {color: '#c7c7c7'} 
+                        gridLines: {
+                            color: '#c7c7c7'
+                        },
+                        categoryPercentage: 0.7,
+                        barThickness: 30
                     }],
                     yAxes: [{ 
                         gridLines: {
@@ -61,11 +82,11 @@ $(function() {
                         },
                         ticks: {
                             beginAtZero: true,
-                            userCallback: function(value, index, values) {
+                            fontColor: '#999',
+                            callback: function(value, index, values) {
                                 value = value.toString();
-                                if( value.length < 4 ){
-                                    
-                                }else {
+                                if( value.length < 4 ) {}
+                                else {
                                     value = value.substring(0 , value.length-3); // 뒤에서 000 세개 제거
                                 }
                                 value = value.split(/(?=(?:...)*$)/);
@@ -77,26 +98,22 @@ $(function() {
                     }]
                 },
                 legend: { 
-                    display: true,
-                    position: 'right',
+                    position: 'top',
                     labels: {
-                        boxWidth: 10
+                        boxWidth: 25,
+                        fontSize: 13,
                     }
                 },
                 tooltips: {
-                    //mode: 'nearest',
-                    backgroundColor: '#8ebc00',
                     callbacks: {
                         title: function() {},
-                        /*label: function(tooltipItem) {
-                            //return Number(tooltipItem.xLabel) + ' : ' + Number(tooltipItem.yLabel).toLocaleString();
-                            return Number(tooltipItem.yLabel).toLocaleString();
-                        }*/
+                        label: function(tooltipItem, data) {
+                            return data.datasets[tooltipItem.datasetIndex].label + ' : ' + Number(tooltipItem.yLabel).toLocaleString();
+                        }
                     }
                 }
             }  
-
+    
         });
     }
-
-});
+}
