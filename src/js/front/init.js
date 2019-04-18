71afce59f1e4bce44d704bd69b1b64c43ad15b3f
vxggangxv <vxggangxv@naver.com>
2019-04-18 10:39:25 +0900

    객실 현황 기능 수정

--- src/js/front/init.js
+++ src/js/front/init.js
@@ -8,19 +8,38 @@ $(function() {
 function rmAreaFn() {
     var rmArea = $("#roomArea");
     // rm = rmArea.finc(".")
-    rmArea.find(".item-floor .floor").on("click", function() {
-        // console.log($(this).text());
-        var flrName = $(this).text();
-        if (flrName == "Total") {
-            rmArea.find(".item-room").fadeIn();
+    // Total 클릭시
+    rmArea.find(".item-floor .floor.total").on("click", function() {
+        rmArea.find(".item-floor .floor").removeClass("on");
+        $(this).addClass("on");
+        rmArea.find(".item-room").fadeIn();
+    });
+    // 층수 클릭시
+    rmArea.find(".item-floor .floor").not(".floor.total").on("click", function() {
+        $(this).toggleClass("on");
+        var flrName = $(this).text(),
+        on_length = rmArea.find(".item-floor .floor.on").size();
+        flrName = "floor-" + flrName;
+        // console.log(on_length);
+        if ( rmArea.find(".item-floor .floor.total").hasClass("on") ) {
+            rmArea.find(".item-room").not(".item-room[ref='"+ flrName +"']").fadeOut();
+            rmArea.find(".item-room[ref='"+ flrName +"']").fadeIn();
+            // total on 경우를 체크하기 위해 뒤에 실행
+            rmArea.find(".item-floor .floor.total").removeClass("on");
+        } else if ( on_length == 0 ) {
+            rmArea.find(".item-floor .floor.total").trigger("click");
         } else {
-            flrName = "floor-" + flrName;
-            // rmArea.find(".item-room").fadeOut();
-            rmArea.find(".item-room").not(".item-room[ref="+ flrName +"]").fadeOut();
-            rmArea.find(".item-room[ref="+ flrName +"]").fadeIn();
+            if ( $(this).hasClass("on") ) {
+                rmArea.find(".item-room[ref='"+ flrName +"']").fadeIn();
+            } else {
+                rmArea.find(".item-room[ref='"+ flrName +"']").fadeOut();
+            }
+            // total on 경우를 체크하기 위해 뒤에 실행
+            rmArea.find(".item-floor .floor.total").removeClass("on");
         }
     });
+    // 룸 클릭시     
     rmArea.find(".list-room > li").on("click", function() {
-        $(this).toggleClass("clk");
+        $(this).toggleClass("on");
     });
 }
