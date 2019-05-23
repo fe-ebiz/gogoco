7bc42df4a765b7bce3edf282b578a93a96e62fb0
vxggangxv <vxggangxv@naver.com>
2019-05-23 18:22:03 +0900

    update

--- src/js/front/init.js
+++ src/js/front/init.js
@@ -43,29 +43,42 @@ function rmAreaFn() {
         $(this).toggleClass("on");
     });
     // 룹 별 마우스 우측 클릭시
+    var roomNo = 0;
     rmArea.find(".list-room > li").on('mousedown', function(e) {
         if (  (event.button == 2) || (event.which == 3) ) {
             // console.log('마우스 오른쪽 클릭 사용 x')
-            var roomNo = $(this).find('.roomNo .num').text();
-            console.log( roomNo );
+            roomNo = $(this).find('.roomNo .num').text();
+            // console.log( roomNo );
             $(document).on('contextmenu', function() {
                 return false;
             });
             var posTop = e.clientY,
                 posLeft = e.clientX;
-            $('.sts-pop').css({
-                "left": posLeft,
-                "top": posTop,
-                "position": "fixed"
-            }).show();
-
+            if ( (posTop + $('.sts-pop').outerHeight() ) > $(window).height() ) {
+                posTop = posTop - $('.sts-pop').outerHeight();
+                $('.sts-pop').css({
+                    "left": posLeft,
+                    "top": posTop,
+                    "position": "fixed"
+                }).show();
+            } else {
+                $('.sts-pop').css({
+                    "left": posLeft,
+                    "top": posTop,
+                    "position": "fixed"
+                }).show();
+            }
         }
     });
     // 상태 팝업 클릭시
-    $('.sts-pop .sts-list > li').on('click', function() {
-        // console.log( $(this).text() );
+
+    $('.sts-pop .sts-list > li').on('click', function(e) {
+        //console.log( roomNo );
+        //console.log( $(this).text() );
         // $(this).closest('.sts-pop').hide();
+		room.status(roomNo, $(e.target).attr('class'));
     });
+
     // 팝업 영역 제외 클릭 시 사라짐
     $(document).on('click', function(e){
         if ( !$(e.target).is('.sts-pop .sts-list > li') ) {
@@ -73,4 +86,22 @@ function rmAreaFn() {
             $('.sts-pop').hide();
         }
     });
+}
+
+
+var room = {
+	status: function(e, i) {
+		$.ajax({		
+			type: 'post',
+			url: './inc/',
+			data: 'mode=room-status&a=' + e + '&b=' + i,
+			success: function(r) {
+				console.log(r);
+				$('.sts-pop').hide();
+			}
+		});
+
+
+		
+	}
 }
