9027d2fdd6a4304752b7ab328aafc1b1d4da0c13
vxggangxv <vxggangxv@naver.com>
2019-08-09 18:11:04 +0900

    finish housekeeper v1.0.0

--- src/js/m/housekeeper/init.js
+++ src/js/m/housekeeper/init.js
@@ -4,16 +4,33 @@ $(function() {
 
 var housekeeperPopup = {
     display : function(flag, obj) {
-        var $obj = $(obj);
+        var $obj = $('[data-target=' + obj + ']');
+        
         switch (flag) {
             case true :
                 $obj.css({display: 'table'});
+                $obj.find('.popup-body').scrollTop(0);
+                wheelFn(false);
                 break;
             case false :
                 $obj.css({display: 'none'});
+                wheelFn(true);
                 break;
             default : 
                 console.log('unhandled flag');
         }
     }
+}
+function wheelFn(flag) {
+    if (flag == false) {
+        $('html').css({
+            overflow: 'hidden'
+        });
+    }
+    if (flag == true) {
+        $('html').css({
+            overflow: 'auto'
+        });
+    }
+
 }
