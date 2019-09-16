861473bc9cf9dd079c6815c9a2bb54c92c13e839
vxggangxv <vxggangxv@naver.com>
2019-09-16 18:07:15 +0900

    update

---
+++ src/js/include/_ipt.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('body').on('click', '.cell.ipt', function() {
+        var _this = $(this),
+            el = '<input type="text" class="form-input">',
+            elVal = '';
+        _this.html(el);
+        _this.find('.form-input').focus();
+        _this.find('.form-input').on('blur', function() {
+            elVal = _this.find('.form-input').val();
+            _this.text(elVal);
+            _this.find('.form-input').remove();
+        });
+        _this.find('.form-input').on('keydown', function(e) {
+            var code = e.keyCode || e.which;
+            if ( code == 13 ) {
+                $(this).blur();
+            }
+        });
+    });
+});
