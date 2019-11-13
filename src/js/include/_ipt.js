6d23e4c5d81971c590759be641ff9e112106e7d8
vxggangxv <vxggangxv@naver.com>
2019-11-13 12:15:46 +0900

    ing ipt

--- src/js/include/_ipt.js
+++ src/js/include/_ipt.js
@@ -1,19 +1,27 @@
 $(function() {
     $('body').on('click', '.cell.ipt', function() {
         var _this = $(this),
+            $no = $('.no');
             el = '<input type="text" class="form-input">',
             elVal = '';
+        $no.hide();
         _this.html(el);
         _this.find('.form-input').focus();
-        _this.find('.form-input').on('blur', function() {
+        _this.find('.form-input').on('change', function() {
             elVal = _this.find('.form-input').val();
             _this.text(elVal);
-            _this.find('.form-input').remove();
+            $no.show();
+            $(this).remove();
+        });
+        _this.find('.form-input').on('blur', function() {
+            $no.show();
+            $(this).remove();
         });
         _this.find('.form-input').on('keydown', function(e) {
             var code = e.which? e.which : e.keyCode;
             if ( code == 13 ) {
                 $(this).blur();
+                console.log(elVal);
             }
         });
     });
